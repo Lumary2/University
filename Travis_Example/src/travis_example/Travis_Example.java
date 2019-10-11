33e00d21b9badf72841af9b54eff9526a0252d36
Lumary2 <bender.anja@gmail.com>
2019-10-11 20:47:02 +0200

    X

---
+++ Travis_Example/src/travis_example/Travis_Example.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travis_example;
+
+/**
+ *
+ * @author anja
+ */
+public class Travis_Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        printJonna();
+    }
+    
+    public static String printJonna(){
+        
+        return "Jonna";
+    }
+    
+}
---
+++ Travis_Example/test/test/Travis_Test.java
@@ -0,0 +1,49 @@
+package test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import travis_example.Travis_Example;
+
+/**
+ *
+ * @author anja
+ */
+public class Travis_Test {
+    
+    public Travis_Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {
+    
+        assertEquals("Jonna", Travis_Example.printJonna());
+    
+    }
+}
